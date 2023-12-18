d95893508a2fe763fb19847134b84d2ad9f85816
Lucas Batista <lucas.bt46@gmail.com>
2023-12-18 07:44:30 -0300

    adicionado style nav-bar

--- frontend-chamados-projeto-ufn/src/app/app.component.ts
+++ frontend-chamados-projeto-ufn/src/app/app.component.ts
@@ -10,29 +10,33 @@ import { filter } from 'rxjs';
 export class AppComponent implements OnInit {
 	title = 'App Chamados';
 
-	constructor(private router: Router) {}
+	constructor(private router: Router) { }
 
 	ngOnInit(): void {
 		this.router.events.pipe(
 			filter(event => event instanceof NavigationEnd)
-		 ).subscribe(() => {
-			
+		).subscribe(() => {
+
 			const routeTitle = this.getTitleFromRoute(this.router.routerState, this.router.routerState.root);
 			console.log(routeTitle);
-			
+
 			this.title = routeTitle || 'App Chamados';
-		 });
+		});
 	}
 
 	private getTitleFromRoute(state: any, parent: any): string | null {
-    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
-      return parent.snapshot.data.title;
-    }
+		if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+			return parent.snapshot.data.title;
+		}
 
-    if (state && parent) {
-      return this.getTitleFromRoute(state, state.firstChild(parent));
-    }
+		if (state && parent) {
+			return this.getTitleFromRoute(state, state.firstChild(parent));
+		}
 
-    return null;
-  }
+		return null;
+	}
+
+	isActive(route: string): boolean {
+		return this.router.isActive(route, true);
+	}
 }
